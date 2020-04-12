f4a6146d92fc3974ba46171d157f48d0b8aa4587
miguel llorens <miguel.llorenscarro@gmail.con>
2020-04-12 11:59:54 +0200

    Añado clase agenda

---
+++ Practica/src/main/java/com/miguel/Practica/Agenda/Agenda.java
@@ -0,0 +1,59 @@
+package com.miguel.Practica.Agenda;
+
+public class Agenda {
+	
+	
+	private String nombre;
+	private String apellidos;
+	private String empresa;
+	private int telefono;
+	private String email;
+	private String direccion;
+	
+	public Agenda(String nombre, String apellidos, String empresa, int telefono, String email, String direccion) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.empresa = empresa;
+		this.telefono = telefono;
+		this.email = email;
+		this.direccion = direccion;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+}
